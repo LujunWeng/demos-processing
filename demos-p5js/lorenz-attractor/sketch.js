54559d362bd9cb0cccd087212b48695dffcc30dc
Lujun Weng <loyweng@163.com>
2016-05-09 16:08:47 +0800

    use babel

--- demos-p5js/lorenz-attractor/sketch.js
+++ demos-p5js/lorenz-attractor/sketch.js
@@ -2,30 +2,40 @@
 /**
  * Created by LujunW on 5/7/2016.
  */
-var sketch = function (p) {
-    var x = 0.01;
-    var y = 0.0;
-    var z = 0.0;
-    var a = 10.0;
-    var b = 28.0;
-    var c = 8.0 / 3.0;
-    p.setup = function () {
-        p.createCanvas(800, 600, 'webgl');
-        p.translate(p.width / 2.0, p.height / 2.0);
-    };
-    p.draw = function () {
-        p.background(0);
-        var dt = 0.01;
-        var dx = (a * (y - x)) * dt;
-        var dy = (x * (b - z) - y) * dt;
-        var dz = (x * y - c * z) * dt;
-        x = x + dx;
-        y = y + dy;
-        z = z + dz;
-        p.rotateX(p.frameCount * 0.01);
-        p.rotateY(p.frameCount * 0.01);
-        p.box(x, y, z);
-    };
-};
-var myp5 = new p5(sketch);
-//# sourceMappingURL=sketch.js.map
+var x, y, z, a, b, c, dt;
+var points;
+
+function setup(){
+    createCanvas(600, 600, WEBGL);
+
+    points = [];
+
+    x = 0.01;
+    y = 0;
+    z = 0;
+
+    a = 10;
+    b = 28;
+    c = 8/3;
+
+    dt = 0.015;
+
+}
+
+function draw(){
+    var dx = (a * (y - x)) * dt;
+    var dy = (x * (b - z) - y) * dt;
+    var dz = (x * y - c * z) * dt;
+
+    x += dx;
+    y += dy;
+    z += dz;
+
+    background(0);
+    fill(255);
+    point(100, 100, 0);
+}
+
+function mousePressed(){
+    setup();
+}
